d4cf3e854e6286e5d15e31eeaf81f823f89f5d2f
Dinesh107 <dineshjr107@gmail.com>
2026-07-21 18:46:38 +0530

    6th commit

--- my-app/src/components/ReactForm.jsx
+++ my-app/src/components/ReactForm.jsx
@@ -2,15 +2,44 @@ import React, { useState } from 'react'
 
 function ReactForm() {
 
-    const [name, setName] = useState("");
+    // const [name, setName] = useState("");
+    // const [age, setAge] = useState("");
+    // const [email, setEmail] = useState("");
 
-   console.log("current state: ", name);
+    const [inputs, setInputs] = useState({email: "@gail.com", age: "22"});
+
+  //  console.log("current state: ", name);
    
+    function handleSubmit(e) {
+      e.preventDefault();
+      console.log("from submitted");
+      console.log("current state: ", inputs);
+    }
+
+    function handleChange(e) {
+ 
+      //      name,age, email
+       const name = e.target.name;
+      const  value = e.target.value;
+
+      // (e) => { setInputs ((previousState) => {  return {...previousState, name: e.target.value }}) }
+      // (e) => { setInputs ( (previousState) => { return {...previousState,  age: e.target.value }}) }
+      // (e) => { setInputs ( (previousState) => { return {...previousState, email: e.target.value }}) }
+    //                                                            
+       setInputs((previousState) => { return {...previousState,  [name]: value }})
+    
+    }
+
   return (
     <>
-      <form action="">
-        <label htmlFor=""> Enter Your name: <input type="text" onChange={(e) => { setName (e.target.value) }}  /> </label>
-        
+      <form action="" onSubmit={handleSubmit}>
+        <label htmlFor=""> Enter Your name: <input type="text" name='name' onChange={handleChange}  /> </label>
+        <br /> <br />
+        <label htmlFor=""> Enter Your age: <input type="text" name='age' onChange={handleChange}   value={inputs.age} /> </label>
+        <br /> <br />
+        <label htmlFor=""> Enter Your email: <input type="text" name='email' onChange={handleChange} value={inputs.email} /> </label>
+         <br /> <br />
+        <input type="submit" value="Submit Form" />
       </form>
     </>
   )
